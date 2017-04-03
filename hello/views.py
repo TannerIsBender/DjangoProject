34dc3ce6db1ad9a961e1eff3daaeeead23ee531b
Tanner Badalato <tannerbadalato13@gmail.com>
2017-04-03 02:17:44 +0000

    Hello App + Server Time

---
+++ hello/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import time
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. The time is: " + time.strftime("%c"))
