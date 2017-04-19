53e3fe1c4d96499708e39072ded4aace2a9a61a3
Tanner Badalato <tannerbadalato13@gmail.com>
2017-04-19 23:20:05 +0000

    Got views started

--- ucp/urls.py
+++ ucp/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'polls'
+app_name = 'ucp'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.UserView.as_view(), name='user'),
+    url(r'^(?P<pk>[0-9]+)/ucp/$', views.UserView.as_view(), name='user'),
 ]
--- ucp/views.py
+++ ucp/views.py
@@ -3,10 +3,14 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 
-from .models import Player
+from .models import Player, User
 
 class IndexView(generic.ListView):
     template_name = 'ucp/index.html'
     context_object_name = 'displayUsers'
     def get_queryset(self):
         return Player.objects.order_by('id')
+        
+class UserView(generic.DetailView):
+    model = User
+    template_name = 'ucp/user.html'
