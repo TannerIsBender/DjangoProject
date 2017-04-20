451ffa76cfe0535b9d35cd9ca300b81016938723
Tanner Badalato <tannerbadalato13@gmail.com>
2017-04-20 20:54:17 +0000

    Admin + Migration Changes to UCP

--- ucp/models.py
+++ ucp/models.py
@@ -13,3 +13,21 @@ class Player(models.Model):
     ipAddress = models.GenericIPAddressField(default=0)
     def __str__(self):
         return self.user.get_username()
+        
+class Ban(models.Model):
+    bannedPlayer = models.CharField(max_length=20)
+    bannedBy = models.CharField(max_length=20)
+    reason = models.TextField(blank=True)
+    canAppeal = models.BooleanField(default=True)
+    timeStamp = models.DateTimeField(auto_now_add=True)
+    
+class Report(models.Model):
+    target = models.CharField(max_length=20)
+    reporter = models.CharField(max_length=20)
+    reason = models.TextField(blank=True)
+    timeStamp = models.DateTimeField(auto_now_add=True)
+    
+class Appeal(models.Model):
+    banID = models.IntegerField(default=0)
+    appealField = models.TextField()
+    timeStamp = models.DateTimeField(auto_now_add=True)
--- ucp/urls.py
+++ ucp/urls.py
@@ -6,5 +6,4 @@ app_name = 'ucp'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.UserView.as_view(), name='user'),
-    url(r'^(?P<pk>[0-9]+)/ucp/$', views.UserView.as_view(), name='user'),
 ]
